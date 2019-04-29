513c247bd7673701e5197eb8dbcf4a00bb8b5587
Ellie Frost <web@stillinbeta.com>
2019-04-29 10:51:13 -0400

    Add seperate step() instruction to CPU

--- src/z80/run.rs
+++ src/z80/run.rs
@@ -17,35 +17,40 @@ impl<'a> Z80<'a> {
             .collect()
     }
 
+    /// Execute a single instruction
+    pub fn step(&mut self) {
+        let mem = self.memory.memory;
+        let pc = self.registers.get_pc();
+        let opcode_horizon = [
+            *mem.get(pc as usize).expect("executed too far!"),
+            mem.get((pc + 1) as usize).map_or(0x00, |i| *i),
+            mem.get((pc + 2) as usize).map_or(0x00, |i| *i),
+            mem.get((pc + 3) as usize).map_or(0x00, |i| *i),
+        ];
+        let (opc, consumed) = opcodes::opcode(opcode_horizon);
+        debug!("Running {:?}", opc);
+        debug!(
+            "A: {:02x}, B: {:02}, C: {:02x}, D: {:02x}, HL: {:04x}, F: {:08b}, PC: {:02x}",
+            self.registers.get_reg8(Reg8::A),
+            self.registers.get_reg8(Reg8::B),
+            self.registers.get_reg8(Reg8::C),
+            self.registers.get_reg8(Reg8::D),
+            self.registers.get_reg16(&Reg16::HL),
+            self.registers.get_reg8(Reg8::F),
+            self.registers.get_pc(),
+        );
+        let pc = self
+            .exec_with_offset(opc) //dbg!(opc))
+            .unwrap_or(pc + consumed as u16);
+        self.registers.set_pc(pc as u16)
+    }
+
     /// Start executing.
     /// The program counter is set to 0x0000, and instructions are executed until a HALT is encountered.
     /// If the program does not contain a HALT, the emulator will simply continue until it runs out of memory.
     pub fn run(&mut self) {
-        let mem = self.memory.memory;
         while !self.is_halted {
-            let pc = self.registers.get_pc();
-            let opcode_horizon = [
-                *mem.get(pc as usize).expect("executed too far!"),
-                mem.get((pc + 1) as usize).map_or(0x00, |i| *i),
-                mem.get((pc + 2) as usize).map_or(0x00, |i| *i),
-                mem.get((pc + 3) as usize).map_or(0x00, |i| *i),
-            ];
-            let (opc, consumed) = opcodes::opcode(opcode_horizon);
-            debug!("Running {:?}", opc);
-            debug!(
-                "A: {:02x}, B: {:02}, C: {:02x}, D: {:02x}, HL: {:04x}, F: {:08b}, PC: {:02x}",
-                self.registers.get_reg8(Reg8::A),
-                self.registers.get_reg8(Reg8::B),
-                self.registers.get_reg8(Reg8::C),
-                self.registers.get_reg8(Reg8::D),
-                self.registers.get_reg16(&Reg16::HL),
-                self.registers.get_reg8(Reg8::F),
-                self.registers.get_pc(),
-            );
-            let pc = self
-                .exec_with_offset(opc) //dbg!(opc))
-                .unwrap_or(pc + consumed as u16);
-            self.registers.set_pc(pc as u16);
+            self.step()
         }
     }
 }
